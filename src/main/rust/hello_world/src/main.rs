091ac043a731464f904f5e4e935951afc1622f73
hisayuki <siruvalice@gmail.com>
2020-06-20 22:21:32 +0900

    helloworld追加

---
+++ src/main/rust/hello_world/src/main.rs
@@ -0,0 +1,34 @@
+use lambda_http::{lambda, IntoResponse, Request};
+use lambda_runtime::{error::HandlerError, Context};
+use serde_json::json;
+
+fn main() {
+    lambda!(handler)
+}
+
+fn handler(
+    _: Request,
+    _: Context,
+) -> Result<impl IntoResponse, HandlerError> {
+    Ok(json!({
+        "message": "AWS Lambda on Rust"
+    }))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn handler_handles() {
+        let request: Request<> = Request::default();
+        let expected = json!({
+            "message": "AWS Lambda on Rust"
+        })
+        .into_response();
+        let response = handler(request, Context::default())
+            .expect("expected Ok(_) value")
+            .into_response();
+        assert_eq!(response.body(), expected.body())
+    }
+}
